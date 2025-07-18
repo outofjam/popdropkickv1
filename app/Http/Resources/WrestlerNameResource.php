5e492f8d96c123eca434fa20d8f8f39eff89be8f
Ismail Dawoodjee <6641797+outofjam@users.noreply.github.com>
2025-07-18 15:13:50 -0400

    remove id from names for cleaner output

--- app/Http/Resources/WrestlerNameResource.php
+++ app/Http/Resources/WrestlerNameResource.php
@@ -7,7 +7,7 @@ class WrestlerNameResource extends BaseResource
     public function toArray($request): array
     {
         return [
-            'id' => $this->resource->id,
+//            'id' => $this->resource->id,
             'name' => $this->resource->name,
             //            'started_at' => $this->formatDate($this->resource->started_at), // Using helper instead of direct call
             //            'ended_at' => $this->formatDate($this->resource->ended_at),     // Using helper instead of direct call
